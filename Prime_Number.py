d2a99e5cc678b5933d406f2b3457b06bd6431033
shreya1 <pritigaikwad148@gmail.com>
2021-01-27 14:53:44 +0530

    check prime number or not

---
+++ Prime_Number.py
@@ -0,0 +1,13 @@
+
+#------------WAP to check number is prime or not_______
+
+num = int(input("enter a number"))
+
+if num > 1 :
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num,"number is not prime number")
+            break
+
+        else:
+            print(num,"number is prime number")
