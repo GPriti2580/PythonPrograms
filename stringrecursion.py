1d1582c4c0853c1687f68cef803b3bc01f530413
shreya1 <pritigaikwad148@gmail.com>
2021-01-27 13:13:52 +0530

    reverse using recursion

---
+++ stringrecursion.py
@@ -0,0 +1,26 @@
+
+
+def reverse (str):
+
+    if len(str) == 0:
+        return str
+    else:
+        return reverse(str[1:])+str[0]
+
+str="hello"
+print(reverse(str))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
